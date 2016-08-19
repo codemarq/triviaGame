15a3093c23ccabb6234e3975f2cd0a6e1ddf857a
John-Mike <jmmarquardt@gmail.com>
2016-08-19 02:14:39 -0400

    js skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	// entire javascript goes here
+
+
+	// global variables
+	// counter
+
+	// score
+
+
+
+
+
+
+	// click handlers
+
+
+	// helper functions
+
+
+
+	// reset
+
+
+	// try to redo it with trivia api
+
+})
